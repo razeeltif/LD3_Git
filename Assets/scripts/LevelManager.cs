ed845ad48d582a98937e9df0a3e1abf48335542f
Valefor <e.berenguier@student.isartdigital.com>
2018-04-23 00:45:40 +0200

    [Add] LevelManager [Modified] MiniGameManager

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public enum STATE { wait, selectedIngredient, play, attack};
+    public STATE statePlayer1 {
+        get { return _statePlayer1; }
+    }
+
+    private STATE _statePlayer1;
+
+    public STATE statePlayer2 {
+        get { return _statePlayer2; }
+    }
+
+    private STATE _statePlayer2;
+
+    public int PLAYER_1 = 1;
+    public int PLAYER_2 = 2;
+
+    // instance du gameManager
+    private static LevelManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        _statePlayer1 = STATE.wait;
+        _statePlayer2 = STATE.wait;
+    }
+
+    public void SetStateWait(int pPlayer) {
+        if (pPlayer == 1) _statePlayer1 = STATE.wait;
+        if (pPlayer == 2) _statePlayer2 = STATE.wait;
+    }
+
+    public void SetStateAttack(int pPlayer) {
+        if (pPlayer == 1) _statePlayer1 = STATE.attack;
+        if (pPlayer == 2) _statePlayer2 = STATE.attack;
+    }
+
+    public void SetStateSelectedIngredient(int pPlayer) {
+        if (pPlayer == 1) _statePlayer1 = STATE.selectedIngredient;
+        if (pPlayer == 2) _statePlayer2 = STATE.selectedIngredient;
+    }
+
+    static public LevelManager getInstance() {
+        return instance;
+    }
+}
--- Assets/scripts/MiniGameManager.cs
+++ Assets/scripts/MiniGameManager.cs
@@ -16,19 +16,29 @@ public class MiniGameManager : MonoBehaviour {
     public GameObject objectActivateMeat;
     public GameObject objectActivateSauce;
 
+    //Variable to know which player is it. Must be defined in the inspector
+    [SerializeField]
+    int _player = 0;
+
+    //LevelManager player state
+    LevelManager.STATE _playerState;
+
     // A l'initialisation, on désactive tous les GameObjects
     void Start () {
         objectActivateVegetable.SetActive(false);
         objectActivatePiment.SetActive(false);
         objectActivateMeat.SetActive(false);
         objectActivateSauce.SetActive(false);
+
+        if (_player == 1) _playerState = LevelManager.getInstance().statePlayer1;
+        if (_player == 2) _playerState = LevelManager.getInstance().statePlayer2;
     }
 	
 	// Update is called once per frame
 	void Update () {
         //Tant qu'un des GameObject est actif (tant que son minijeu est actif), on ne peux pas lancer un autre minijeu
         //A la fin du minijeu, le GameObject se désactive, permettant d'activer un autre GameObject (et donc un autre minijeu)
-        if(objectActivateVegetable.activeSelf == false && objectActivatePiment.activeSelf == false && 
+        /*if(objectActivateVegetable.activeSelf == false && objectActivatePiment.activeSelf == false && 
               objectActivateMeat.activeSelf == false && objectActivateSauce.activeSelf == false)
         {
             //Si on appuie sur "inputLauncherMeat", on active le GameObject "objectActivateMeat"
@@ -54,6 +64,33 @@ public class MiniGameManager : MonoBehaviour {
             if (Input.GetKeyDown(inputLauncherSauce))
             {
                 objectActivateSauce.SetActive(true);
+            }
+        }*/
+
+        if (_playerState == LevelManager.STATE.wait) {
+            //Si on appuie sur "inputLauncherMeat", on active le GameObject "objectActivateMeat"
+            //Et le GameObject activé permet de lancer son script de jeu
+            if (Input.GetKeyDown(inputLauncherMeat)) {
+                objectActivateMeat.SetActive(true);
+                LevelManager.getInstance().SetStateSelectedIngredient(_player);
+            }
+            //Si on appuie sur "inputLauncherVegetables", on active le GameObject "objectActivateVegetable"
+            //Et le GameObject activé permet de lancer son script de jeu
+            if (Input.GetKeyDown(inputLauncherVegetables)) {
+                objectActivateVegetable.SetActive(true);
+                LevelManager.getInstance().SetStateSelectedIngredient(_player);
+            }
+            //Si on appuie sur "inputLauncherPiment", on active le GameObject "objectActivatePiment"
+            //Et le GameObject activé permet de lancer son script de jeu
+            if (Input.GetKeyDown(inputLauncherPiment)) {
+                objectActivatePiment.SetActive(true);
+                LevelManager.getInstance().SetStateSelectedIngredient(_player);
+            }
+            //Si on appuie sur "inputLauncherSauce", on active le GameObject "objectActivateSauce"
+            //Et le GameObject activé permet de lancer son script de jeu
+            if (Input.GetKeyDown(inputLauncherSauce)) {
+                objectActivateSauce.SetActive(true);
+                LevelManager.getInstance().SetStateSelectedIngredient(_player);
             }
         }
     }
