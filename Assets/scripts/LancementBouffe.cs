b3bf8c15b391372669e92ac5fb6fc97452974afa
Unknown <razeltif@gmail.com>
2018-04-24 00:30:26 +0200

    touché aux prefabs et main1

--- Assets/scripts/LancementBouffe.cs
+++ Assets/scripts/LancementBouffe.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LancementBouffe : MonoBehaviour {
 
 	public GameObject tomateFarciePrefab;
+
 	public GameObject assiettePrefab;
+
+	public float thrust;
+
+	public Vector3 positionDeDepart;
+
+	public Vector3 rotationObjet;
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.B)){
+			Debug.Log("yolo");
+			GameObject obj = Instantiate(assiettePrefab);
+			obj.transform.position = new Vector3(-4.5f, 1, -0.3f);
+			obj.transform.rotation = Quaternion.Euler(-25, 90, 0);
+		}
 	}
 }
